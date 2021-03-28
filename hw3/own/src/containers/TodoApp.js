9332870bdd0000bbedde1ccba5f4cf84dc43fc19
Tommy787576 <tommy787576@gmail.com>
2021-03-28 13:17:15 +0800

    Update 2021.3.28 finshed

--- hw3/own/src/containers/TodoApp.js
+++ hw3/own/src/containers/TodoApp.js
@@ -1,32 +1,54 @@
 import React, { Component } from "react";
 import Header from "../components/Header";
 import TodoList from "./TodoList";
-import Footer from "../components/Footer";
+import Footer from "./Footer";
 
 class TodoApp extends Component {
-    constructor() {
-        super();
-        this.state = { todoList: [] };
+    constructor(props) {
+        super(props);
+        this.state = { todoList: [], mode: "All" };
+    }
+    deleteItem = (idx) => {
+        let currList = this.state.todoList;
+        const mode = this.state.mode;
+
+        currList.splice(idx, 1);
+        this.setState({ todoList: currList, mode });
     }
     setIsFinished = (idx) => {
         let currList = this.state.todoList;
+        const mode = this.state.mode;
 
         currList[idx].isFinished = !currList[idx].isFinished;
-        this.setState({ todoList: currList });
+        this.setState({ todoList: currList, mode });
     }
     handleKeyDown = (event) => {
         if (event.key === "Enter") {
             let currList = this.state.todoList;
+            const mode = this.state.mode;
             const id = new Date().getTime();
             const todoItem = event.target.value;
 
             currList.push({ id, todoItem, isFinished: false });
-            this.setState({ todoList: currList });
+            this.setState({ todoList: currList, mode });
 
             // reset to default value
             event.target.value = "";
             event.target.placeholder = "What needs to be done?";
         }
+    }
+    changeMode = (newMode) => {
+        const currList = this.state.todoList;
+
+        this.setState({ todoList: currList, mode: newMode });
+    }
+    deleteCompleted = () => {
+        const mode = this.state.mode;
+        const currList = this.state.todoList;
+        const newList = currList.filter(element => !element.isFinished);
+
+        // console.log(newList);
+        this.setState({ todoList: newList, mode });
     }
     render() {
         return (
@@ -34,9 +56,9 @@ class TodoApp extends Component {
                 <Header text="todos" />
                 <section className="todo-app__main">
                     <input className="todo-app__input" placeholder="What needs to be done?" onKeyDown={this.handleKeyDown} />
-                    <TodoList todoList={this.state.todoList} setIsFinished={this.setIsFinished} />
+                    <TodoList mode={this.state.mode} todoList={this.state.todoList} setIsFinished={this.setIsFinished} deleteItem={this.deleteItem} />
                 </section>
-                <Footer todoList={this.state.todoList} />
+                <Footer todoList={this.state.todoList} mode={this.state.mode} changeMode={this.changeMode} deleteCompleted={this.deleteCompleted} />
             </>
         );
     }
--- hw3/own/src/containers/TodoList.js
+++ hw3/own/src/containers/TodoList.js
@@ -3,51 +3,50 @@ import React, { Component } from "react";
 class TodoList extends Component {
     constructor(props) {
         super(props);
-        this.todoList = this.props.todoList;
         this.setIsFinished = this.props.setIsFinished;
+        this.deleteItem = this.props.deleteItem;
     }
     handleClick = (event) => {
         let labelNode = event.target;
-        let h1Node = event.target.parentNode.nextSibling;
-
         const labelNodeId = parseInt(labelNode.id);
-        let foundIdx = -1;
-        for (let i = 0; i < this.todoList.length; i++) {
-            if (this.todoList[i].id === labelNodeId) {
-                foundIdx = i;
-                break;
-            }
-        }
+        const foundIdx = this.props.todoList.findIndex((element) => element.id === labelNodeId);
 
         // toggle color of the label
         if (foundIdx !== -1) {
-            if (!this.todoList[foundIdx].isFinished) {
-                labelNode.style.background = "rgba(37, 211, 46, 0.698)";
-                h1Node.style.textDecoration = "line-through";
-                h1Node.style.opacity = 0.5;
-                this.setIsFinished(foundIdx);
-            }
-            else {
-                labelNode.style.background = "rgba(99, 99, 99, 0.698)";
-                h1Node.style.textDecoration = "";
-                h1Node.style.opacity = "";
-                this.setIsFinished(foundIdx);
-            }
+            this.setIsFinished(foundIdx);
         }
+    }
+    handleDelete = (event) => {
+        const labelNode = event.target.previousSibling.previousSibling.childNodes[0];
+        const id = parseInt(labelNode.id);
+        const idx = this.props.todoList.findIndex((element) => element.id === id);
+
+        this.deleteItem(idx);
     }
     render() {
         return (
-            this.todoList.map((element) =>
-                <li key={element.id} className="todo-app__item">
-                    <div className="todo-app__checkbox" onClick={this.handleClick}>
-                        <label key={element.id} id={element.id} />
-                    </div>
-                    <h1 className="todo-app__item-detail">
-                        {element.todoItem}
-                    </h1>
-                    <img src="./img/x.png" className="todo-app__item-x" alt="cross"></img>
-                </li>
-            )
+            this.props.todoList.map((element) => {
+                if (this.props.mode === "Active" && element.isFinished)
+                    return (null);
+                else if (this.props.mode === "Completed" && !element.isFinished)
+                    return (null);
+                else {
+                    const labelStyle = element.isFinished ? { background: "rgba(37, 211, 46, 0.698)" } : { background: "rgba(99, 99, 99, 0.698)" };
+                    const h1Style = element.isFinished ? { textDecoration: "line-through", opacity: 0.5 } : { textDecoration: "", opacity: "" };
+
+                    return (
+                        <li key={element.id} className="todo-app__item">
+                            <div className="todo-app__checkbox" onClick={this.handleClick}>
+                                <label key={element.id} id={element.id} style={labelStyle} />
+                            </div>
+                            <h1 className="todo-app__item-detail" style={h1Style}>
+                                {element.todoItem}
+                            </h1>
+                            <img src="./img/x.png" className="todo-app__item-x" alt="cross" onClick={this.handleDelete}></img>
+                        </li>
+                    )
+                }
+            })
         )
     }
 }
