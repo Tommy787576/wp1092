59d794000677e4de10e94f5c703ff29c2dcf4517
ShihPingLin <shihping.lin.tw@gmail.com>
2021-04-19 03:38:53 +0800

    Add 2021/4/19

--- hw4/own/src/containers/ContentTable.js
+++ hw4/own/src/containers/ContentTable.js
@@ -1,81 +1,23 @@
-import React, { useState } from "react";
+import React from "react";
 
 function ContentTable(props) {
-    const [prevRow, setPrevRow] = useState("0");
-    const [prevCol, setPrevCol] = useState("0");
-    const [clickTimes, setClickTimes] = useState(0);
-    const [contentArray, setContent] = useState([...Array(101)].map(() => Array(27).fill("")));
+    const { handleKeyDown, handleClick, handleOnChange, numRow, numCol, contentArray } = props;
+    let rowIndex = Array.from({ length: numRow }, (_, i) => i + 1);
+    let colIndex = Array.from({ length: numCol }, (_, i) => i + 1);
 
-    const focusIdx = (event) => {
-        const [row, col] = event.target.id.split("_");
-        const prevBlock = document.getElementById(prevRow + "_" + prevCol);
-        const prevRowBlock = document.getElementById(prevRow + "_0");
-        const prevColBlock = document.getElementById("0_" + prevCol);
-        const currBlock = document.getElementById(row + "_" + col);
-        const currRowBlock = document.getElementById(row + "_0");
-        const currColBlock = document.getElementById("0_" + col);
-
-        prevBlock.style.border = "";
-        prevRowBlock.style.backgroundColor = "#fffafa";
-        prevColBlock.style.backgroundColor = "#fffafa";
-
-        currBlock.style.border = "2px solid blue";
-        currRowBlock.style.backgroundColor = "#ece9e9";
-        currColBlock.style.backgroundColor = "#ece9e9";
-
-        if (row === prevRow && col === prevCol)
-            setClickTimes(2);
-        else
-            setClickTimes(1);
-        setPrevRow(row);
-        setPrevCol(col);
-    }
-
-    const handleKeyDown = (event) => {
-        if (clickTimes === 2) {
-            const keyAscii = event.keyCode;
-            const [row, col] = event.target.id.split("_");
-            const currBlock = document.getElementById(row + "_" + col);
-
-            if ((keyAscii >= 48 && keyAscii <= 57) ||
-                (keyAscii >= 97 && keyAscii <= 122) ||
-                (keyAscii >= 65 && keyAscii <= 90) ||
-                (keyAscii === 8 || keyAscii === 46)) {
-                let temp = "";
-
-                if (currBlock.innerHTML) {
-                    if (currBlock.innerText.charAt(currBlock.innerText.length - 1) === '|')
-                        temp = currBlock.innerText.slice(0, -1);
-                    else
-                        temp = currBlock.innerText;
-                    currBlock.innerHTML = "";
-                    currBlock.innerText = "";
-                }
-
-                if (temp !== "" && (event.key === "Backspace" || event.key === "Delete"))
-                    currBlock.innerText = temp.slice(0, -1);
-                else if (!(event.key === "Backspace" || event.key === "Delete"))
-                    currBlock.innerText = temp + event.key;
-                contentArray[prevRow][prevCol] = currBlock.innerText;
-
-                setContent(contentArray);
-            }
-            currBlock.innerHTML += "<span class='blinking-cursor'>|</span>"
-        }
-    }
-
-    let rowIndex = Array.from({ length: props.numRow }, (_, i) => i + 1);
-    let colIndex = Array.from({ length: props.numCol }, (_, i) => i + 1);
     let contentTable = rowIndex.map((idxRow) => {
         const contentRow = colIndex.map((idxCol) =>
             <td
-                id={idxRow.toString() + "_" + idxCol.toString()}
                 key={idxRow.toString() + "_" + idxCol.toString()}
-                tabIndex="1"
-                onClick={focusIdx}
-                onKeyDown={handleKeyDown}
+                onClick={handleClick}
             >
-                {contentArray[idxRow][idxCol]}
+                <input
+                    id={idxRow.toString() + "_" + idxCol.toString()}
+                    type="text"
+                    onKeyDown={handleKeyDown}
+                    onChange={handleOnChange}
+                    value={contentArray[idxRow][idxCol]}
+                ></input>
             </td>
         )
 
--- hw4/own/src/containers/FakeSheet.js
+++ hw4/own/src/containers/FakeSheet.js
@@ -1,11 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import ContentTable from "./ContentTable"
 import HeadIndex from "../components/HeadIndex"
 
-let numRow = 100;
-let numCol = 26;
-
 function FakeSheet() {
+    const [numRow, setNumRow] = useState(100);
+    const [numCol, setNumCol] = useState(26);
+    const [prevRow, setPrevRow] = useState("0");
+    const [prevCol, setPrevCol] = useState("0");
+    const [contentArray, setContent] = useState([...Array(101)].map(() => Array(27).fill("")));
+
+    const handleClick = (event) => {
+        if (event.target.tagName === "INPUT") {
+            const [row, col] = event.target.id.split("_");
+            const currInput = document.getElementById(row + "_" + col);
+            switchBlock(row, col);
+
+            currInput.select();
+
+            if (row !== prevRow || col !== prevCol) {
+                setPrevRow(row);
+                setPrevCol(col);
+            }
+        }
+    }
+
+    const handleKeyDown = (event) => {
+        if (event.key === "Enter") {
+            const [row, col] = [parseInt(prevRow), parseInt(prevCol)];
+
+            if ((row + 1) <= numRow) {
+                switchBlock('' + (row + 1), '' + col);
+                setPrevRow('' + (row + 1));
+
+                const currInput = document.getElementById((row + 1) + "_" + col);
+                currInput.focus();
+            }
+        }
+    }
+
+    const handleOnChange = (event) => {
+        const currInput = document.getElementById(prevRow + "_" + prevCol)
+        let copy = [...contentArray];
+        copy[prevRow][prevCol] = currInput.value;
+        setContent(copy);
+    }
+
+    const handleAddAbove = () => {
+        console.log("in");
+        let copy = [...contentArray];
+        let currNumRow = numRow;
+        let currRow = prevRow + 1;
+
+        console.log(copy[prevRow][prevCol]);
+        // console.log(copy);
+        copy.splice(prevRow, 0, Array(numCol).fill(""));
+        console.log(copy);
+
+        switchBlock(currRow, prevCol);
+        setNumRow(currNumRow + 1);
+        setContent(copy);
+        setPrevRow(currRow);
+    }
+
+    const switchBlock = (row, col) => {
+        const prevBlock = document.getElementById(prevRow + "_" + prevCol).parentNode;
+        const prevRowBlock = document.getElementById(prevRow + "_0");
+        const prevColBlock = document.getElementById("0_" + prevCol);
+        const currBlock = document.getElementById(row + "_" + col).parentNode;
+        const currRowBlock = document.getElementById(row + "_0");
+        const currColBlock = document.getElementById("0_" + col);
+
+        prevBlock.style.border = "";
+        prevRowBlock.style.backgroundColor = "#fffafa";
+        prevColBlock.style.backgroundColor = "#fffafa";
+
+        currBlock.style.border = "2px solid blue";
+        currRowBlock.style.backgroundColor = "#ece9e9";
+        currColBlock.style.backgroundColor = "#ece9e9";
+    }
+
     return (
         <>
             <div id="top-button">
@@ -14,7 +87,7 @@ function FakeSheet() {
             </div>
             <div id="main-content">
                 <div id="left-button">
-                    <button>+</button>
+                    <button onClick={handleAddAbove}>+</button>
                     <button>-</button>
                 </div>
                 <table>
@@ -22,7 +95,14 @@ function FakeSheet() {
                         <HeadIndex numCol={numCol} />
                     </thead>
                     <tbody>
-                        <ContentTable numRow={numRow} numCol={numCol} />
+                        <ContentTable
+                            handleKeyDown={handleKeyDown}
+                            handleClick={handleClick}
+                            handleOnChange={handleOnChange}
+                            numRow={numRow}
+                            numCol={numCol}
+                            contentArray={contentArray}
+                        />
                     </tbody>
                 </table>
             </div>
