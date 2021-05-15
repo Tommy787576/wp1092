d3e1ff2b1b9fe93b9d9c87d82509846ec0fc129a
Tommy787576 <tommy787576@gmail.com>
2021-05-15 18:04:34 +0800

    Update hw6

--- hw6/own/frontend/src/hooks/useScoreCard.js
+++ hw6/own/frontend/src/hooks/useScoreCard.js
@@ -7,9 +7,9 @@ const ERROR_MESSAGE_COLOR = '#fb3640';
 const ScoreCardContext = createContext({
   messages: [],
 
-  addCardMessage: () => {},
-  addRegularMessage: () => {},
-  addErrorMessage: () => {},
+  addCardMessage: () => { },
+  addRegularMessage: () => { },
+  addErrorMessage: () => { },
 });
 
 const makeMessage = (message, color) => {
@@ -20,18 +20,17 @@ const ScoreCardProvider = (props) => {
   const [messages, setMessages] = useState([]);
 
   const addCardMessage = (message) => {
-    setMessages([...messages, makeMessage(message, ADD_MESSAGE_COLOR)]);
+    setMessages([makeMessage(message, ADD_MESSAGE_COLOR)]);
   };
 
   const addRegularMessage = (...ms) => {
     setMessages([
-      ...messages,
       ...ms.map((m) => makeMessage(m, REGULAR_MESSAGE_COLOR)),
     ]);
   };
 
   const addErrorMessage = (message) => {
-    setMessages([...messages, makeMessage(message, ERROR_MESSAGE_COLOR)]);
+    setMessages([makeMessage(message, ERROR_MESSAGE_COLOR)]);
   };
 
   return (
