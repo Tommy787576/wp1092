46dcd93fc40b553e19300ee68eb8d11eb853609a
Tommy787576 <tommy787576@gmail.com>
2021-06-05 23:40:38 +0800

    Add hw7 ver1

---
+++ hw7/own/frontend/src/Containers/ChatRoom.js
@@ -0,0 +1,112 @@
+import "../App.css";
+import ChatModal from "../Components/ChatModal";
+import useChatBox from "../hooks/useChatBox";
+import useChat from "../hooks/useChat";
+import { useState } from "react";
+import { Tabs, Input } from "antd";
+
+const { TabPane } = Tabs;
+const ChatRoom = ({ me, displayStatus }) => {
+    const [messageInput, setMessageInput] = useState("");
+    const [modalVisible, setModalVisible] = useState(false);
+    const [activeKey, setActiveKey] = useState("");
+    const { chatBoxes, createChatBox, removeChatBox } = useChatBox();
+    const { sendMessage, startChat, messages } = useChat(me);
+
+    const addChatBox = () => { setModalVisible(true); };
+
+    return (
+        <> <div className="App-title">
+            <h1>{me}'s Chat Room</h1> </div>
+            <div className="App-messages">
+                <Tabs
+                    type="editable-card"
+                    activeKey={activeKey}
+                    onChange={(key) => {
+                        setActiveKey(key);
+                        console.log("change to", key);
+                        startChat(key);
+                    }}
+                    onEdit={(targetKey, action) => {
+                        if (action === "add")
+                            addChatBox();
+                        else if (action === "remove") {
+                            const newActiveKey = removeChatBox(targetKey, activeKey);
+                            setActiveKey(newActiveKey);
+                            startChat(newActiveKey);
+                        }
+                    }}
+                >
+                    {chatBoxes.map((
+                        { friend, key, chatLog }) => {
+                        return (
+                            <TabPane tab={friend} key={key} closable={true}>
+                                {messages.length === 0 ? (
+                                    <p>{friend}'s chatbox.</p>
+                                ) : (
+                                    messages.map(({ name, body }, i) => {
+                                    if (name === me) {
+                                        return (
+                                            <p className="App-message" key={i}>
+                                                <span className="message-sender">{name}</span> 
+                                                <span className="message-text">{body}</span>
+                                            </p>
+                                        )
+                                    }
+                                    else {
+                                        return (
+                                            <p className="App-message" style={{textAlign: "right"}} key={i}>
+                                                <span className="message-text">{body}</span> 
+                                                <span className="message-sender">{name}</span>
+                                            </p>
+                                        )
+                                    }
+                                })
+                              )}
+                            </TabPane>
+                        );
+                    })}
+                </Tabs>
+                <ChatModal
+                    visible={modalVisible}
+                    onCreate={({ name }) => {
+                        setActiveKey(createChatBox(name, me));
+                        setModalVisible(false);
+                        startChat(name);
+                    }}
+                    onCancel={() => {
+                        setModalVisible(false);
+                    }}
+                />
+            </div>
+                <Input.Search
+                    value={messageInput}
+                    onChange={(e) =>
+                        setMessageInput(e.target.value)}
+                    enterButton="Send"
+                    placeholder=
+                    "Enter message here..."
+                    onSearch={(msg) => {
+                        if (!msg) {
+                            displayStatus({
+                                type: "error",
+                                msg: "Please enter message.",
+                            });
+                            return;
+                        } else if (activeKey === "") {
+                            displayStatus({
+                                type: "error",
+                                msg: "Please add a chatbox first.",
+                            });
+                            setMessageInput("");
+                            return;
+                        }
+                        sendMessage({ key: activeKey, body: msg });
+                        setMessageInput("");
+                    }}
+
+                ></Input.Search>
+        </>);
+};
+export default ChatRoom;
+
---
+++ hw7/own/frontend/src/hooks/useChat.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+const server = new WebSocket('ws://localhost:4000');
+
+const useChat = (me) => {
+    const [status, setStatus] = useState({});
+    const [messages, setMessages] = useState([]);
+
+    server.onopen = () => console.log('Server connected.');
+    server.onmessage = (m) => {
+        onEvent(JSON.parse(m.data));
+    };
+    server.sendEvent = (e) => server.send(JSON.stringify(e));
+
+    const startChat = (key) => {
+        server.sendEvent({
+            type: 'CHAT',
+            data: { to: key, name: me },
+        });
+    };
+
+    const sendMessage = (payload) => {
+        server.sendEvent({
+            type: 'MESSAGE',
+            data: { to: payload.key, name: me, body: payload.body },
+        });
+    };
+
+    const onEvent = (e) => {
+        const { type } = e;
+
+        switch (type) {
+            case 'CHAT': {
+                setMessages(() => e.data.messages);
+                break;
+            }
+            case 'MESSAGE': {
+                setMessages(() => [...messages, e.data.message])
+                break;
+            }
+        }
+    }
+
+    return { status, sendMessage, startChat, messages };
+};
+export default useChat;
